1f9e68eb4cfc37230334050507e903bfd7f09553
Bodil Stokke <bodil@bodil.org>
2014-10-30 16:24:18 +0000

    Eq impl for Pair needs rounding too. :(

--- src/Data/Geom.purs
+++ src/Data/Geom.purs
@@ -1,9 +1,13 @@
 module Data.Geom where
 
+import Math
+
 data Pair = Pair Number Number
 
 instance eqPair :: Eq Pair where
-  (==) (Pair x1 y1) (Pair x2 y2) = (x1 == x2) && (y1 == y2)
+  (==) (Pair x1 y1) (Pair x2 y2) =
+    let sortOf a b = round (a * 10000) == round (b * 10000)
+    in sortOf x1 x2 && sortOf y1 y2
   (/=) a b = not (a == b)
 
 addPairs :: Pair -> Pair -> Pair
